a9e504cf2dd4551a76b7338e082c02a8550414b1
suneelponkam <104679140+suneelponkam@users.noreply.github.com>
2022-11-17 01:22:35 +0530

    Create app1.js

---
+++ src/app1.js
@@ -0,0 +1,9 @@
+import './App.css';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+
+function App1() {
+  return <ImageSlider slides={SliderData} />;
+}
+
+export default App1;
