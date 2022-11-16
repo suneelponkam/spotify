ef419114af9607481116c485e80c5a2a71a4891a
suneelponkam <104679140+suneelponkam@users.noreply.github.com>
2022-11-17 01:22:53 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+
 const GridWrapper = styled.div`
-  display: grid;
-  grid-gap: 10px;
+  display: list;
+  grid-gap: 30px;
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
   grid-template-columns: repeat(12, 1fr);
   grid-auto-rows: minmax(25px, auto);
+
 `;
 export const Home = (props) => (
   <GridWrapper>
